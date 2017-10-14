97015e751190b13ac1b42a40e54e2a861b28c45d
stardenis <starshenkov@gmail.com>
2017-10-14 15:05:23 +0300

    completed with bootstrap flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def nav_link(link_text, link_path, link_class, li_class)
+    if request.fullpath == link_path
+      class_name = "#{li_class} active";
+    elsif link_path != '/' && request.fullpath.start_with?(link_path)
+      class_name = "#{li_class} active";
+    else
+      class_name = li_class;
+    end
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path, :class => link_class
+    end
+  end
+
 end
