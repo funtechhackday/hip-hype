03a20e575f820ef09c02030f0b427206779a7ac8
stardenis <starshenkov@gmail.com>
2017-10-14 18:40:31 +0300

    added welcome controller and views for web part

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
+  scope module: :web do
+    root to: 'welcome#index'
+  end
+
   namespace :admin do
     root to: 'users#index'
     resources :users, only: [:index, :edit, :update]
   end
+  
 end
