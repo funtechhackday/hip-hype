d9e3edfb20612a35e001d2c7f1ab5085dcd3296e
stardenis <starshenkov@gmail.com>
2017-10-15 12:32:51 +0300

    WIP refile for upload

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -21,6 +21,6 @@ class Admin::UsersController < Admin::ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name)
+    params.require(:user).permit(:first_name, :last_name, :avatar)
   end
 end
